a74c4b6e75f0345346bc71607d08866cab0cd563
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2020-03-23 17:03:18 -0400

    Add lesson 2 for all trainees.

---
+++ src/main/java/programming/school/julia/Lesson2Patterns.java
@@ -0,0 +1,32 @@
+/*
+ * What is this?
+ */
+package programming.school.julia;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import programming.school.DrawingContainer;
+import programming.school.IDrawingInstructions;
+import programming.school.Utilities;
+
+public class Lesson2Patterns implements IDrawingInstructions {
+
+	private void setRandomColor(Graphics2D g) {
+		g.setColor(new Color(Utilities.randomNumber(255), Utilities.randomNumber(255), Utilities.randomNumber(255)));
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		for (int i = 0; i < 1000; i++) {
+			setRandomColor(g);
+			g.drawLine(i, 0, 1000 - i, 1000);
+			g.drawLine(0, i, 1000, 1000 - i);
+		}
+	}
+
+	public static void main(String[] args) {
+		new DrawingContainer(new Lesson2Patterns()).runContainer(args);
+	}
+
+}
