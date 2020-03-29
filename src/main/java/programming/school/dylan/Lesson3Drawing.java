cd54774fd0a1897eb11ee9aa10df101ba9b9ca44
Dylan Ecimovic <dylan@localhost.localdomain>
2020-03-29 16:04:06 -0400

    Dylan's Donut Shop

--- src/main/java/programming/school/dylan/Lesson3Drawing.java
+++ src/main/java/programming/school/dylan/Lesson3Drawing.java
@@ -1,10 +1,11 @@
 package programming.school.dylan;
 
-import java.awt.Graphics2D;
+import java.awt.*;
 
 import programming.school.DrawingContainer;
 import programming.school.IDrawingInstructions;
 import programming.school.IKeyboardAction;
+import programming.school.Utilities;
 
 public class Lesson3Drawing implements IDrawingInstructions, IKeyboardAction {
 
@@ -19,30 +20,46 @@ public class Lesson3Drawing implements IDrawingInstructions, IKeyboardAction {
 	@Override
 	public void draw(Graphics2D g) {
         plotDot(g);
+        g.drawString("Dylan's Donut Shop", 600, 800);
 	}
 
     @Override
     public void keyPressed(Graphics2D g, char ch) {
         switch(ch) {
             case 'a': 
-                x-=3;
+                x-=100;
+                size++;
                 break;
             case 'd':
-                x+=3;
+                x+=100;
+                size++;
                 break;
             case 's':
-                y+=3;
+                y+=100;
+                size++;
                 break;
             case 'w':
-                y-=3;
+                y-=100;
+                size++;
                 break;
+            case 'n':
+                size = 10;
+                break;
+            case 'f':
+                setRandomColor(g);
+                    break;
+                
         }
         plotDot(g);
     }
+	private void setRandomColor(Graphics2D g) {
+		g.setColor(new Color(Utilities.randomNumber(255), Utilities.randomNumber(255), Utilities.randomNumber(255)));
+	}
 
 	public static void main(String[] args) {
         Lesson3Drawing l = new Lesson3Drawing();
         new DrawingContainer(l,l).runContainer(args);
+  
 	}
 
 }
