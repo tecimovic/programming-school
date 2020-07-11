a0b8b33dad49db47b90a0851b8adb367108d5592
Dylan Ecimovic <dylan@localhost.localdomain>
2020-07-11 13:45:03 -0400

    Tunnels

---
+++ src/main/java/programming/school/dylan/Lesson5CircleRoom.java
@@ -0,0 +1,49 @@
+package programming.school.dylan;
+
+import java.awt.*;
+
+import programming.school.DrawingContainer;
+import programming.school.IDrawingInstructions;
+import programming.school.IKeyboardAction;
+
+public class Lesson5CircleRoom implements IDrawingInstructions, IKeyboardAction {
+
+    private int size = 10;
+
+    private int x = 100;
+
+    private int y = 200;
+
+    public void keyPressed(final Graphics2D g, final char ch) {
+        switch (ch) {
+            case 'w':
+               g.setColor(Color.black); size++;
+                break;
+            case 's':
+              g.setColor(Color.white);  size--;
+                break;
+            case 'a':
+            g.setColor(Color.black); x--;
+                break;
+            case 'd':
+            g.setColor(Color.black); x++;
+                break;
+            case 'o':
+            g.setColor(Color.black); y++;
+                break;
+            case 'l':
+            g.setColor(Color.black); y--;
+
+        }
+    }
+
+    public void draw(final Graphics2D g) {
+        g.drawOval(x, y, size, size);
+
+    }
+
+    public static void main(final String[] args) {
+        Lesson5CircleRoom l = new Lesson5CircleRoom();
+        new DrawingContainer(l, l, true).runContainer(args);
+    }
+}
