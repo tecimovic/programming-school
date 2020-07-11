4fa51ca4992337eee25ff8e4f1e0a6bdefbde5bc
Dylan Ecimovic <dylan@localhost.localdomain>
2020-07-11 10:43:27 -0400

    Dylan's Donuts Are Tasty

--- src/main/java/programming/school/dylan/Lesson4ScreenSaver.java
+++ src/main/java/programming/school/dylan/Lesson4ScreenSaver.java
@@ -12,7 +12,7 @@ public class Lesson4ScreenSaver implements IDrawingInstructions, IKeyboardAction
 
     private int speed = 3;
     private final Random rnd = new Random();
-
+    
     private int x = rnd.nextInt(DrawingContainer.MAX_X);
     private int y = rnd.nextInt(DrawingContainer.MAX_Y);
     private int vx = speed;
@@ -42,12 +42,15 @@ public class Lesson4ScreenSaver implements IDrawingInstructions, IKeyboardAction
 	@Override
 	public void draw(final Graphics2D g) {
         reset();
-        switch(rnd.nextInt(3)) {
+        switch(rnd.nextInt(6)) {
             case 0: g.setColor(Color.BLUE); break;
             case 1: g.setColor(Color.RED); break;
             case 2: g.setColor(Color.GREEN); break;
+            case 3: g.setColor(Color.CYAN); break;
+            case 4: g.setColor(Color.MAGENTA); break;
+            case 5: g.setColor(Color.YELLOW); break;
         }
-        g.drawOval(x, y, 20, 20);
+        g.drawOval(x, y, 10 + rnd.nextInt(40), 20);
 	}
 
     @Override
