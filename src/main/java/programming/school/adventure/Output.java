5ce1158393b965d57b343c463d046792deb12957
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2020-07-18 13:42:25 -0400

    Move some methods around to where they belong.

--- src/main/java/programming/school/adventure/Output.java
+++ src/main/java/programming/school/adventure/Output.java
@@ -1,6 +1,8 @@
 package programming.school.adventure;
 
 import java.io.PrintStream;
+import java.util.List;
+import java.util.Set;
 
 public class Output {
   private PrintStream out;
@@ -25,5 +27,60 @@ public class Output {
     out.println("\n-----------------------------------------------------------------");
   }
 
+  public void describePlace(Place place) {
+    out.println(place.description());
+    out.println();
+    out.println(creatureInventory(place));
+    out.println();
+    out.println("You see: " + inventoryDescription(place.inventory()));
+    out.println("You can go: " + directionDescription(place.directions()));
+  }
+
+  private static String creatureInventory(Place place) {
+    StringBuilder sb = new StringBuilder();
+    Creature[] creaturesHere = place.creatures();
+    if ( creaturesHere.length == 0 ) {
+      sb.append("There is nobody else here.");
+    } else if ( creaturesHere.length == 1 ) {
+      sb.append("There is " + creaturesHere[0].name() + " here.");
+    } else {
+      sb.append("There are ");
+      for ( int i=0; i<creaturesHere.length; i++ ) {
+        if ( i == creaturesHere.length-1 ) {
+          sb.append(" and ");
+        } else if ( i > 0 ) {
+          sb.append(", ");
+        }
+        sb.append(creaturesHere[i].name());
+      }
+      sb.append(" here.");
+    }
+    return sb.toString();
+  }
+
+  public static String inventoryDescription(final List<Thing> list) {
+    if (list.size() == 0)
+      return "nothing.";
+  
+    StringBuilder sb = new StringBuilder();
+    String prefix = "";
+    for (Thing t : list) {
+      sb.append(prefix).append(t.name());
+      prefix = ", ";
+    }
+    sb.append(".");
+  
+    return sb.toString();
+  }
+
+  private static String directionDescription(final Set<String> directions) {
+    StringBuilder sb = new StringBuilder();
+    String prefix = "";
+    for (String d : directions) {
+      sb.append(prefix).append(d);
+      prefix = ", ";
+    }
+    return sb.toString();
+  }
 
 }
--- src/main/java/programming/school/adventure/Player.java
+++ src/main/java/programming/school/adventure/Player.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Player {
 
@@ -134,7 +133,7 @@ public class Player {
   }
 
   public void inventory() {
-    out.println("You carry: " + inventoryDescription(inventory));
+    out.println("You carry: " + Output.inventoryDescription(inventory));
   }
 
   public void examine(final String argument) {
@@ -167,53 +166,6 @@ public class Player {
     }
   }
 
-  public static String directionDescription(final Set<String> directions) {
-    StringBuilder sb = new StringBuilder();
-    String prefix = "";
-    for (String d : directions) {
-      sb.append(prefix).append(d);
-      prefix = ", ";
-    }
-    return sb.toString();
-  }
-
-  private static String creatureInventory(Place place) {
-    StringBuilder sb = new StringBuilder();
-    Creature[] creaturesHere = place.creatures();
-    if ( creaturesHere.length == 0 ) {
-      sb.append("There is nobody else here.");
-    } else if ( creaturesHere.length == 1 ) {
-      sb.append("There is " + creaturesHere[0].name() + " here.");
-    } else {
-      sb.append("There are ");
-      for ( int i=0; i<creaturesHere.length; i++ ) {
-        if ( i == creaturesHere.length-1 ) {
-          sb.append(" and ");
-        } else if ( i > 0 ) {
-          sb.append(", ");
-        }
-        sb.append(creaturesHere[i].name());
-      }
-      sb.append(" here.");
-    }
-    return sb.toString();
-  }
-  
-  private static String inventoryDescription(final List<Thing> list) {
-    if (list.size() == 0)
-      return "nothing.";
-
-    StringBuilder sb = new StringBuilder();
-    String prefix = "";
-    for (Thing t : list) {
-      sb.append(prefix).append(t.name());
-      prefix = ", ";
-    }
-    sb.append(".");
-
-    return sb.toString();
-  }
-
   public void processText(IAdventureGame game, String line) {
     String l = line.trim();
 
@@ -236,12 +188,7 @@ public class Player {
 
     while (state() == PlayerState.NORMAL) {
       out.separate();
-      out.println(place().description());
-      out.println();
-      out.println(creatureInventory(place()));
-      out.println();
-      out.println("You see: " + inventoryDescription(place().inventory()));
-      out.println("You can go: " + directionDescription(place().directions()));
+      out.describePlace(place());
       out.prompt();
       String text = in.nextLine();
       processText(game, text);
