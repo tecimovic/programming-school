cc69eea907a1a31c08cd765c8cb361d33985a3f8
Dylan Ecimovic <dylan@localhost.localdomain>
2020-08-04 16:34:52 -0400

    waterslide1

--- src/main/java/programming/school/student/dylan/Lesson7NewAdventure.java
+++ src/main/java/programming/school/student/dylan/Lesson7NewAdventure.java
@@ -12,9 +12,12 @@ public class Lesson7NewAdventure implements IAdventureGame {
   private final Place your_room = new Place("You are in a cruise, and you are in your room.");
   private final Place hallway = new Place("You are in the main hallway of the cruise.");
   private final Place pool_room = new Place("You see a pool. About 10ft deep.");
-  private final Place waterslides = new Place("You see 3 waterslides. They all lead into the pool.");
+  private final Place waterslides = new Place("You see 3 waterslides. They all lead into the pool. Number 1 is labled MILD. Number 2 is labled WILD. Number 3 is labled RIDICULOUS.");
   private final Place restaraunt = new Place("Welcome to the restaraunt. Buy food here.");
   private final Place pool = new Place("You are swimming in the pool. If you don't have a mask, you can't go more than 1ft deep.");
+  private final Place mildenterance = new Place("You are in a normal waterslide. It is dark in here...");
+  private final Place mildending = new Place("You are still in the slide. You can see some light...");
+
 
   public Lesson7NewAdventure() {
       //Link places
@@ -29,8 +32,14 @@ public class Lesson7NewAdventure implements IAdventureGame {
   pool_room.addDirection("JUMP", pool);
 
   waterslides.addDirection("downstairs", pool_room);
+  waterslides.addDirection("in mild slide", mildenterance);
 
   pool.setSound(getClass().getResource("splash.wav"));
+  pool.addDirection("out", pool_room);
+
+  mildenterance.addDirection("continue slide", mildending);
+
+  mildending.addDirection("continue", pool);
   }
 
   @Override
@@ -45,7 +54,7 @@ public class Lesson7NewAdventure implements IAdventureGame {
 
   @Override
   public String introductionText() {
-    return "Welcome to the survival game of Villabibadushi. I hope u enjoYYYYYYYYYYYg";
+    return "Welcome to the survival game of Inflatable. I hope u enjoYYYYYYYYYYYg";
   }
 
   @Override
