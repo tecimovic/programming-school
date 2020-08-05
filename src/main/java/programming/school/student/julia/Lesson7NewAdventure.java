e2ba8c4a7a60eabc6048ea6a24aa111f3640c76d
Julia Ecimovic <julia@localhost.localdomain>
2020-08-05 13:12:45 -0400

    qazaw3sedrce

--- src/main/java/programming/school/student/julia/Lesson7NewAdventure.java
+++ src/main/java/programming/school/student/julia/Lesson7NewAdventure.java
@@ -14,15 +14,23 @@ public class Lesson7NewAdventure implements IAdventureGame {
   private final Place highway = new Place("it is a highway from hull to boston");
   private final Place vautrinot_avenue = new Place("the street you live on that has alot of potholes");
   private final Place home = new Place("the house you live in that is on vautriot ave");
+  private final Place dunkin_donuts = new Place("a normal dunkin' donuts that is out of strawberry donuts");
 
   public Lesson7NewAdventure() {
+    train_station.addDirection("north", train);
+    train_station.addDirection("south",  highway);
+    train_station.addDirection("east",  dunkin_donuts);
+    highway.addDirection("north", train_station);
+    highway.addDirection("south",  vautrinot_avenue);
+    vautrinot_avenue.addDirection("southeast",  home);
+    vautrinot_avenue.addDirection("north",  highway);
   }
 
   @Override
   public String playerName() {
-    return "mr. qazwax";
+    return "proffesser. qwerty qazwax";
   }
-
+ 
   @Override
   public Place startingPlace() {
     return train_station;
@@ -30,7 +38,8 @@ public class Lesson7NewAdventure implements IAdventureGame {
 
   @Override
   public String introductionText() {
-    return "welcome to train game. in this game you have to get .";
+    return "welcome to train game. in this game you have to get the train ticket to go on the train to anchorage
+    .";
   }
 
   @Override
