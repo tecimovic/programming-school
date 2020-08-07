0282c50149d04d5881f7434e751816d9b66f426d
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2020-08-07 14:30:49 -0400

    House,  improved.

--- src/main/java/programming/school/student/timotej/Lesson7NewAdventure.java
+++ src/main/java/programming/school/student/timotej/Lesson7NewAdventure.java
@@ -1,5 +1,6 @@
 package programming.school.student.timotej;
 
+import programming.school.adventure.Creature;
 import programming.school.adventure.IAdventureGame;
 import programming.school.adventure.IOutput;
 import programming.school.adventure.Place;
@@ -34,6 +35,19 @@ public class Lesson7NewAdventure implements IAdventureGame {
   private final Place guestbedroom = new Place("You are in a guest bedroom.");
   private final Place attic = new Place("You are in the attic.");
   private final Place upstairsDeck = new Place("Upper deck.");
+  
+  // Creatures
+  private final Creature fluffy = new Creature("Fluffy", "A cute little guinea pig, black and brown and a little bit white.");
+  private final Creature cotton = new Creature("Cotton", "A cute little guinea pig, four-colored one.");
+  private final Creature cloudy = new Creature("Cloudy", "A cute little guinea pig, white and a little bit brown.");
+  
+  private final Creature rocky = new Creature("Rocky", "A chicken. Somewhat brownish with gray on her head.");
+  private final Creature snowflake = new Creature("Snowflake", "A chicken. White, with a little brown patch on the head.");
+  private final Creature syrup = new Creature("Syrup", "A chicken, black and brown.");
+  private final Creature sirchirps = new Creature("Sir Chirps", "A chicken, black and brown.");
+  
+  private final Creature daddylonglegs = new Creature("Daddy Longlegs", "Daddy long legs of an alien type.");
+
   
   public Lesson7NewAdventure() {
     attic.addDirection("down", guestbedroom);
@@ -104,6 +118,18 @@ public class Lesson7NewAdventure implements IAdventureGame {
     
     kitchen.addDirection("playroom", playroom);
     kitchen.addDirection("living room", livingRoom);
+    
+    // Creature placement
+    livingRoom.addCreature(fluffy);
+    livingRoom.addCreature(cloudy);
+    livingRoom.addCreature(cotton);
+    
+    basement.addCreature(daddylonglegs);
+    
+    chickenland.addCreature(rocky);
+    chickenland.addCreature(sirchirps);
+    chickenland.addCreature(snowflake);
+    chickenland.addCreature(syrup);
   }
 
   @Override
