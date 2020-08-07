28486530ae2a7aaed88f58875a290bf744a05718
Julia Ecimovic <julia@localhost.localdomain>
2020-08-07 14:07:41 -0400

    qwerty commit

--- src/main/java/programming/school/student/julia/Lesson7NewAdventure.java
+++ src/main/java/programming/school/student/julia/Lesson7NewAdventure.java
@@ -4,10 +4,10 @@ import programming.school.adventure.IAdventureGame;
 import programming.school.adventure.IOutput;
 import programming.school.adventure.Place;
 import programming.school.adventure.Player;
+import programming.school.adventure.Store;
 import programming.school.adventure.ui.GameUi;
 
 public class Lesson7NewAdventure implements IAdventureGame {
-
   // Create places
   private final Place train_station = new Place("You are in a train station. There is a dunkin' donuts.");
   private final Place train = new Place("it is a train from boston to ancorage");
@@ -15,6 +15,9 @@ public class Lesson7NewAdventure implements IAdventureGame {
   private final Place vautrinot_avenue = new Place("the street you live on that has alot of potholes");
   private final Place home = new Place("the house you live in that is on vautriot ave");
   private final Place dunkin_donuts = new Place("a normal dunkin' donuts that is out of strawberry donuts");
+  private final Place exit_27 = new Place("a normal highway exit");
+  
+
 
   public Lesson7NewAdventure() {
     train_station.addDirection("north", train);
@@ -22,9 +25,16 @@ public class Lesson7NewAdventure implements IAdventureGame {
     train_station.addDirection("east",  dunkin_donuts);
     highway.addDirection("north", train_station);
     highway.addDirection("south",  vautrinot_avenue);
+    highway.addDirection("west",  exit_27);
     vautrinot_avenue.addDirection("southeast",  home);
     vautrinot_avenue.addDirection("north",  highway);
-  }
+    dunkin_donuts.addDirection("west", train_station);
+     
+    Store storeExtension = new Store();
+    
+    
+    // Add place extensions
+    dunkin_donuts.addExtension(storeExtension); }
 
   @Override
   public String playerName() {
@@ -45,6 +55,7 @@ public class Lesson7NewAdventure implements IAdventureGame {
   public String victoryText() {
     return "You are happy!";
   }
+  
 
   @Override
   public void evaluateState(final Player player, final IOutput out) {
