355e99e444cd400c0813ac2b3d9ddab28b8a4eb6
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2020-08-29 13:29:17 -0400

    Add media support for creatures.

--- src/main/java/programming/school/adventure/Creature.java
+++ src/main/java/programming/school/adventure/Creature.java
@@ -1,10 +1,16 @@
 package programming.school.adventure;
 
+import java.net.URL;
+
 public class Creature implements IExaminable {
 
   private String name, description;
   private Place place = null;
   
+
+  private URL picture = null;
+  private URL sound = null;
+
   public Creature(String name, String description) {
     this.name = name;
     this.description = description;
@@ -25,4 +31,31 @@ public class Creature implements IExaminable {
   public Place place() {
     return place;
   }
+  
+
+  public void setPicture(URL url) {
+    this.picture = url;
+  }
+
+  public boolean hasPicture() {
+    return picture != null;
+  }
+
+  public URL picture() {
+    return picture;
+  }
+
+  public void setSound(URL url) {
+    this.sound = url;
+  }
+
+  public boolean hasSound() {
+    return sound != null;
+  }
+
+  public URL sound() {
+    return sound;
+  }
+
+
 }
