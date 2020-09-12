a27caa512bebc733503721a98d4b9fd34786bd69
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2020-09-12 13:07:01 -0400

    Remove stray copyright.

--- src/main/java/programming/school/adventure/IAdventureGame.java
+++ src/main/java/programming/school/adventure/IAdventureGame.java
@@ -1,5 +1,3 @@
-// Copyright (c) 2020 Silicon Labs. All rights reserved.
-
 package programming.school.adventure;
 
 public interface IAdventureGame {
@@ -9,12 +7,14 @@ public interface IAdventureGame {
   public String playerName();
   public String introductionText();
   public String victoryText();
+  
   // Various possible callbacks.
   public default boolean allowGoShortcut() { return false; }
   public default void thingAdded(Player player, Thing t, IOutput out) {}
   public default void thingRemoved(Player player, Thing t, IOutput out) {}
   public default void creatureAction(Player player, Creature creature, Place place, IOutput out) {}
   public default boolean canPlayerMove(Player player, Place from, Place to, IOutput out) {return true;}
+  
   public default String currencyDescription(int value) {
     StringBuilder ret = new StringBuilder();
     if ( value == 0 ) 
