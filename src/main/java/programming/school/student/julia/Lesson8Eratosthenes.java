b18db02f1ea74d6f74d557a0cd329a5a72aa3a6a
Julia Ecimovic <julia@localhost.localdomain>
2021-10-26 13:45:53 -0400

    Kazwhack

--- src/main/java/programming/school/student/julia/Lesson8Eratosthenes.java
+++ src/main/java/programming/school/student/julia/Lesson8Eratosthenes.java
@@ -19,19 +19,36 @@ public class Lesson8Eratosthenes implements IDrawingInstructions {
     int i = 0;
     int EDGE_X = 50;
     int EDGE_Y = 50;
-    g.setColor(Color.RED);
-    
-    for (int x = 0; x < MAX_X; x++) {
-      for (int y = 0; y < MAX_Y; y++) {
+    g.setColor(Color.BLUE);
+
+    for (int n = 1; n <= 500 * 500; n++) {
+      int x = n % 500;
+      int y = n / 500;
+      // for (int x = 0; x < MAX_X; x++) {
+      // for (int y = 0; y < MAX_Y; y++) {
+      // if(y==33 || y==66 || y==99|| x==66 || x==33 || x==99)
+      if ((x + y) % 20 == 0)
+
         plotDot(EDGE_X + x, EDGE_Y + y, g);
-      }
     }
+
   }
 
- 
   public static void main(String[] args) {
     Lesson8Eratosthenes l = new Lesson8Eratosthenes();
     new DrawingContainer(l).runContainer(args);
   }
 
 }
+
+// trrrrruytrcv
+
+// qazwax
+
+// rar
+
+// warren piece
+
+// hue hue
+
+//euh euh
