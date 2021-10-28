6930305164f298c41b16a357b4cce29f7fd13cf0
Julia Ecimovic <julia@localhost.localdomain>
2021-10-28 13:05:00 -0400

    This Is My Commit

--- src/main/java/programming/school/student/julia/Lesson8Eratosthenes.java
+++ src/main/java/programming/school/student/julia/Lesson8Eratosthenes.java
@@ -12,6 +12,7 @@ public class Lesson8Eratosthenes implements IDrawingInstructions {
     g.drawOval(x - 1, y - 1, 1, 1);
   }
 
+  // @Override
   @Override
   public void draw(Graphics2D g) {
     int MAX_X = 500;
@@ -21,19 +22,23 @@ public class Lesson8Eratosthenes implements IDrawingInstructions {
     int EDGE_Y = 50;
     g.setColor(Color.BLUE);
 
-    for (int n = 2*2; n <= 500 * 500; n+=2) {
-      int x = n % 500;
-      int y = n / 500;
-      // for (int x = 0; x < MAX_X; x++) {
-      // for (int y = 0; y < MAX_Y; y++) {
-      // if(y==33 || y==66 || y==99|| x==66 || x==33 || x==99)
-      //if (y%6 == 0)
-     //if (n%2==0 || n%3==0 || n%5==0 ||  n%7==0 || n%11==0)
+    // int prime = 20;
 
+    for (int prime = 2; prime < MAX_Y * MAX_X / 2; prime++) {
+
+      for (int n = 2 * prime; n <= 500 * 500; n += prime) {
+        int x = n % 500;
+        int y = n / 500;
+        // for (int x = 0; x < MAX_X; x++) {
+        // for (int y = 0; y < MAX_Y; y++) {
+        // if(y==33 || y==66 || y==99|| x==66 || x==33 || x==99)
+        // if (y%6 == 0)
+        // if (n%2==0 || n%3==0 || n%5==0 || n%7==0 || n%11==0)
 
         plotDot(EDGE_X + x, EDGE_Y + y, g);
-    }
+      }
 
+    }
   }
 
   public static void main(String[] args) {
@@ -53,6 +58,42 @@ public class Lesson8Eratosthenes implements IDrawingInstructions {
 
 // hue hue
 
-//euh euh
+// euh euh
 
-//vxcgcyufbgwrwnwxroyyxgyggnvuyxyuxvyuxyvuuxvyu3wxuuxvvxvywwxx
+// vxcgcyufbgwrwnwxroyyxgyggnvuyxyuxvyuxyvuuxvyu3wxuuxvvxvywwxx
+
+// har har har
+
+// trrrrruytrcv
+
+// qazwax
+
+// rar
+
+// warren piece
+
+// hue hue
+
+// euh euh
+
+// vxcgcyufbgwrwnwxroyyxgyggnvuyxyuxvyuxyvuuxvyu3wxuuxvvxvywwxx
+
+// har har har
+
+// trrrrruytrcv
+
+// qazwax
+
+// rar
+
+// warren piece
+
+// hue hue
+
+// euh euh
+
+// vxcgcyufbgwrwnwxroyyxgyggnvuyxyuxvyuxyvuuxvyu3wxuuxvvxvywwxx
+
+// har har har
+
+//Draw String 
