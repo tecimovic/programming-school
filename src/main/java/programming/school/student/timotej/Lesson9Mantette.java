84e018acb4a49e2a350ba7fdbfb8f8f6064210c3
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2022-07-24 19:00:35 -0400

    Add 3 rooms.

--- src/main/java/programming/school/student/timotej/Lesson9Mantette.java
+++ src/main/java/programming/school/student/timotej/Lesson9Mantette.java
@@ -8,11 +8,20 @@ import programming.school.adventure.ui.GameUi;
 
 public class Lesson9Mantette implements IAdventureGame {
 
-    private Place hole = new Place("A hole.");
+    private Place hole = new Place("Dark wompy hole.");
+    private Place ogreLand = new Place("A smelly pit of nonsense where ogre lives!");
+    private Place spinnerLand = new Place("Kaching, looks like Las Vegas in here! Wowzeee!!!!");
+    private Place goblinLand = new Place("Horror show of smellyness and evilness.");
+
+    public Lesson9Mantette() {
+        hole.addDirection("Blue Door", ogreLand);
+        hole.addDirection("Red Door", spinnerLand);
+        hole.addDirection("Green Door", goblinLand);
+    }
 
     @Override
     public String introductionText() {
-        return "Wuga wuga.";
+        return "Wuga wuga, you are in a horrible land of ogres and goblins. Sometimes you can even spin a spinner for luck. Try to live to tell the tale!";
     }
 
     @Override
