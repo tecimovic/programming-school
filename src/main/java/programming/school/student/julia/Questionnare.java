9fc8b72a3e9e5db3f5c7f49f6cbaa827fe055ac1
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2022-08-01 17:05:56 -0400

    Next project.

---
+++ src/main/java/programming/school/student/julia/Questionnare.java
@@ -0,0 +1,13 @@
+package programming.school.student.julia;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Questionnare {
+    private List<Question> questions = new ArrayList<>();
+}
+
+class Question {
+    private String question;
+    private String answer;
+}
