301f02acfc1690449ffca761ffddc11177152483
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2023-02-20 12:00:48 -0500

    Quiz.

---
+++ src/main/java/programming/school/student/timotej/A1000DollarQuizz.java
@@ -0,0 +1,23 @@
+package programming.school.student.timotej;
+
+import java.util.Random;
+
+public class A1000DollarQuizz {
+    public static void main(String[] args) {
+        System.out.println("Starting...");
+
+        Random rnd = new Random();
+
+        int[] counters = new int[13];
+        for (int i = 0; i < 100000; i++) {
+            int die1 = 1 + rnd.nextInt(6);
+            int die2 = 1 + rnd.nextInt(6);
+            counters[die1 + die2]++;
+        }
+
+        for (int i = 0; i < 13; i++) {
+            System.out.println(i + ": " + counters[i]);
+        }
+        System.out.println("Done.");
+    }
+}
