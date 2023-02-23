04af9cba9f5970cf9fc6ebae2f94cb4a23ca5754
NeonScripter <62514986+NeonScripter@users.noreply.github.com>
2023-02-23 11:02:46 -0500

    hw

--- src/main/java/programming/school/student/dylan/howtowin1000dollars.java
+++ src/main/java/programming/school/student/dylan/howtowin1000dollars.java
@@ -3,23 +3,24 @@ package programming.school.student.dylan;
 import java.util.Random;
 
 public class howtowin1000dollars {
-// dicey                  dice
+// dicey                  dice                     throw the number 20000
     public static void main(String[] args) {
         System.out.println("SPINSPIN SPINTHEDICE MAKE IT MAKE IT MAKE IT NICE YOU WILL WIN 1000 DOLLARS WELLMAYBE NOT NECICARILY BUT SPINIT SPINIT SPINIT 100000 TIMES!!!!!!!!!!!!!!!!!!!!!!!!!!");
         //TODDLER FUN
         Random rnd = new Random();
         //TODDLER FUN
-        System.out.println("DICEY DICEY DICEY ROLLED, DICEY DICEY DICEY IS " + (1 + rnd.nextInt(6)) + " AND THE OTHER DICEY DICE DECIDED IT IS " + (1 + rnd.nextInt(6)));
-        int[] counter = new int[13];
-        for(int c=0; c<100000; c++){
+        System.out.println("DICEY DICEY DICEY ROLLED, DICEY DICEY DICEY IS " + (1 + rnd.nextInt(6)) + (1+ rnd.nextInt(6)));
+        int[] counter = new int[19];
+        for(int c=0; c<200000; c++){
             int die1= rnd.nextInt(6)+1;
             int die2= rnd.nextInt(6)+1;
-            counter[die1+die2]++;
+            int die3= rnd.nextInt(6)+1;
+            counter[die1+die2+die3]++;
 
 
         }
 
-        for(int c=0; c<13;c++) {
+        for(int c=0; c<19;c++) {
         System.out.println(
             "number " + c
             + " rolled " + counter[c]
