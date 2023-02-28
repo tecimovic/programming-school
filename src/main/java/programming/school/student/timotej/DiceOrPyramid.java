2490c4243c8c60bed82000bbae4851d70ac10429
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2023-02-28 12:11:55 -0500

    Dice or pyramid.

---
+++ src/main/java/programming/school/student/timotej/DiceOrPyramid.java
@@ -0,0 +1,42 @@
+package programming.school.student.timotej;
+
+import java.util.Random;
+
+public class DiceOrPyramid {
+
+    public static void main(String[] args) {
+        System.out.println("Game of dice and pyramids....");
+        Random rnd = new Random();
+        final int maxCount = 18;
+        final int rollCount = 1000000;
+
+        int[] pyramidCounts = new int[maxCount + 1];
+        int[] diceCounts = new int[maxCount + 1];
+
+        for (int box = 0; box <= maxCount; box++) {
+
+            for (int roll = 0; roll < rollCount; roll++) {
+
+                int totalDiceCount = 0;
+                for (int i = 0; i < 3; i++) {
+                    totalDiceCount += 1 + rnd.nextInt(6);
+                }
+
+                int totalPyramidCount = 0;
+                for (int i = 0; i < 18; i++) {
+                    totalPyramidCount += rnd.nextInt(2);
+                }
+
+                if (totalDiceCount == box)
+                    diceCounts[box]++;
+
+                if (totalPyramidCount == box) {
+                    pyramidCounts[box]++;
+                }
+
+            }
+
+            System.out.println("Box " + box + ": pyramids: " + pyramidCounts[box] + " / dice: " + diceCounts[box]);
+        }
+    }
+}
