ad4779791b40ae893db74e2055ad6705d74f279c
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2023-04-13 11:46:04 -0400

    Fix a problem.

--- src/main/java/programming/school/student/dylan/Relentless.java
+++ src/main/java/programming/school/student/dylan/Relentless.java
@@ -1,12 +1,16 @@
 package programming.school.student.dylan;
 
+import programming.school.adventure.Creature;
 import programming.school.adventure.IAdventureGame;
+import programming.school.adventure.IOutput;
 import programming.school.adventure.Place;
+import programming.school.adventure.Player;
+import programming.school.adventure.Thing;
 
 public class Relentless implements IAdventureGame {
-    private Place miami = new Place("You are in Miami. Will you go to the beach?");
-}
-public String playerName() {
+  private Place miami = new Place("You are in Miami. Will you go to the beach?");
+
+  public String playerName() {
     return "Lost soul";
   }
 
@@ -36,3 +40,10 @@ public String playerName() {
   @Override
   public void creatureAction(Player player, Creature creature, Place place, IOutput out) {
   }
+
+  @Override
+  public void evaluateState(Player player, IOutput out) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'evaluateState'");
+  }
+}
