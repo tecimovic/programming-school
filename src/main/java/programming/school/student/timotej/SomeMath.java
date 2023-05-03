faa1d8e7d1da0f436fb35accba278d05c3c792f3
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2023-05-03 12:00:10 -0400

    Some simple math.

---
+++ src/main/java/programming/school/student/timotej/SomeMath.java
@@ -0,0 +1,21 @@
+package programming.school.student.timotej;
+
+public class SomeMath {
+
+    private static double radToDegrees(double rad) {
+        return (180.0 * rad / Math.PI);
+    }
+
+    private static double degreeToRad(double degree) {
+        return (Math.PI * degree / 180.0);
+    }
+
+    public static void main(String[] args) {
+        double x;
+
+        for (x = 0.0; x <= 360.0; x += 1.0) {
+            System.out.println("sin(" + x + ") = " + Math.sin(degreeToRad(x)));
+            System.out.println("cos(" + x + ") = " + Math.cos(degreeToRad(x)));
+        }
+    }
+}
