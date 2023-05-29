5c41af0c1254851aa005635e01af1ecd2097688e
Julia <julia.ecimovic@gmail.com>
2023-05-29 10:27:55 -0400

    Changes.

---
+++ src/main/java/programming/school/student/julia/drawgonometweedotjava.java
@@ -0,0 +1,41 @@
+package programming.school.student.julia;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.PrintGraphics;
+
+import org.checkerframework.checker.units.qual.g;
+
+import programming.school.framework.DrawingContainer;
+import programming.school.framework.IDrawingInstructions;
+
+public class drawgonometweedotjava implements IDrawingInstructions {
+    private static int SIZE_X = 360;
+    private static int SIZE_Y = 200;
+    @Override
+    public void draw(Graphics2D g) {
+        int a;
+        double e;
+        
+        g.setColor(Color.BLUE);
+       // g.drawLine(0, 200, 360, 200);
+      //  g.drawLine(360, 0, 360, 200);
+        g.setColor(Color.BLACK);
+        g.drawString("PLEASE ZOOM IN SO YOU SEE THE FRAMED AREA", 500, 500);
+        g.setColor(Color.ORANGE);
+        //g.drawLine(0, 230, 390, 230);
+       // g.drawLine(390, 0, 390, 230);
+       
+        for (a=0;a<360;a++) {
+            e = (Math.sin(a)*200.0+100.0);
+             g.drawOval(a, (int)Math.round (e), 2, 2 );
+        }
+    }
+        
+    public static void main(String[] args) {
+        
+        
+        new DrawingContainer(new drawgonometweedotjava()).runContainer(args);
+        
+    }
+}
---
+++ src/main/java/programming/school/student/julia/mathrghuirghuzhg.java
@@ -0,0 +1,19 @@
+package programming.school.student.julia;
+
+public class mathrghuirghuzhg {
+    private static double radToDegrees(double rad) {
+        return (180.0 * rad / Math.PI);
+    }
+
+    private static double degreeToRad(double degree) {
+        return (Math.PI * degree / 180.0);
+    }
+    public static void main(String[] args) {
+       int a;
+        System.out.println("QWERTYUIOPASDFGHJKLZXCVBNM");
+        System.out.println("WELCOME TO CVHEJRLAND");
+        for (a=0;a<360;a++) {
+            System.out.println("sine of "+a+" is "+Math.sin (degreeToRad(a))+", and the cosine is "+Math.cos (degreeToRad(a))+".");
+        }
+    }
+}
