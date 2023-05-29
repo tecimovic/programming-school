5429eb9fa963dfd8513ac84c25847fa68ea87430
Timotej Ecimovic <timotej.ecimovic@silabs.com>
2023-05-29 10:04:21 -0400

    Add a new lesson.

---
+++ src/main/java/programming/school/framework/IPlottingInstructions.java
@@ -0,0 +1,15 @@
+package programming.school.framework;
+
+import java.awt.Graphics2D;
+
+/**
+ * Implementation of drawing instructions.
+ * 
+ * @author timotej
+ *
+ */
+public interface IPlottingInstructions {
+
+	public void draw(Graphics2D g);
+
+}
