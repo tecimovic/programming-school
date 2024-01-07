f523a4aa38fd1a7c75ddc681a2fbc0962949408e
Julia <julia.ecimovic@gmail.com>
2024-01-07 12:44:24 -0500

    i changed something :) :) :)

--- src/main/java/programming/school/student/julia/mathrghuirghuzhg.java
+++ src/main/java/programming/school/student/julia/mathrghuirghuzhg.java
@@ -12,7 +12,7 @@ public class mathrghuirghuzhg {
        int a;
         System.out.println("QWERTYUIOPASDFGHJKLZXCVBNM");
         System.out.println("WELCOME TO CVHEJRLAND");
-        for (a=0;a<360;a++) {
+        for (a=0;a<361;a+=10) {
             System.out.println("sine of "+a+" is "+Math.sin (degreeToRad(a))+", and the cosine is "+Math.cos (degreeToRad(a))+".");
         }
     }
